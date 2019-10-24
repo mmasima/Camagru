e74e85ade7ec763b3917a165f45a9d529f579f3b
Morapedi MASIMA <mmasima@c4r22s2.wethinkcode.co.za>
2019-10-24 12:12:31 +0200

    login is kind of working

---
+++ view/login.php
@@ -0,0 +1,45 @@
+<?php
+    include '../controller/authenticate.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<style>
+body{
+    padding: 0px;
+    margin: 0px;
+    background: skyblue;
+}
+input{
+    padding: 10px;
+}
+</style> 
+<body>
+    <form action="login.php" method = "post" >
+        <table align = "center"  bgcolor="lightblue" width=400>
+            <tr align = center>
+                <td colspan = "8">
+                <h1>CAMAGRU</h1>
+                    <h2>Login here!</h2>
+                </td>
+            </tr>
+            <tr>
+                 <td align = "right"><strong>Email :</strong></td>
+                 <td><input type="text" name="user_email" placeholder="enter your email" required ="required"></td>
+            </tr>
+            <tr>
+                 <td align = "right"><strong>Password :</strong></td>
+                 <td><input type="password" name="user_password" placeholder="enter your password" required ="required"></td>
+            </tr>
+             <td align = "center" colspan = "8"><input type= "submit" name = "Login" value= "Login"></td>
+        </table>
+    </form>
+    <center><h3>Not a User?<a href= "register.php">register here</a></h3></center>
+</body>
+</html>
